244d179c23d06617ea8bef05f26c506c933c0f51
LucaGerbaudo <44701649+LucaGerbaudo@users.noreply.github.com>
2021-11-29 12:35:51 +0100

    Fixed decimals to create directories.

--- iou_conf_combo.py
+++ iou_conf_combo.py
@@ -98,18 +98,24 @@ if __name__ == "__main__":
     
     start_time = time.time()
     
-    i = 0
+    k = 1
+    i_conf = 0.0
     # conf iou in [0.0, 1.0] -> step 0.1 --> 121 run (11 x 11)
-    for i_conf in np.arange(0.0,1.1,0.1):
-        my_options.conf_thres = round(i_conf)
-        for i_iou in np.arange(0.0,1.1,0.1):
-            print(f"\n\n\t***CONF: {i_conf} - IOU: {i_iou}***\n\n")
+    for i in range(11):
+        my_options.conf_thres = i_conf
+        i_iou = 0.0
+        for j in range(11):
+            print(f"\t{k}***CONF: {i_conf} - IOU: {i_iou}***")
             my_options.iou_thres = round(i_iou,1)
             # Edit directory name
             my_options.name = f"conf_{i_conf}-iou_{i_iou}"
             # Run on the current combination of thresholds
             val.main(my_options)
-            i+=1
+            i_iou += 0.1
+            i_iou = round(i_iou,1)
+            k+=1
+        i_conf += 0.1
+        i_conf = round(i_conf,1)
     
     print(f"\n\n\t**** Total time of execution save date of {i} combinations of conf e iou: %s. ****\n\n" %
           str(datetime.timedelta(seconds=(time.time() - start_time))))
